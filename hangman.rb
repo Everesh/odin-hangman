a33c714c64e8500ca7f8d9ac4a5e3059f21d23a4
Everesh <everesh@proton.me>
2024-05-12 23:17:37 +0200

    Implement evaluate

--- hangman.rb
+++ hangman.rb
@@ -2,6 +2,8 @@ class Hangman
 
   ATTEMPTS = 8
 
+  attr_reader :state, :wrong_guesses, :attempts_left
+
   def initialize
     raise 'Failed to initialize, dictionary.txt does not exist' unless File.exist?('./dictionary.txt')
 
@@ -12,9 +14,9 @@ class Hangman
     @attempts_left = ATTEMPTS
   end
 
-  def guess(letter)
+  def guess(str)
     begin
-      letter = letter.downcase.chr
+      letter = str.downcase.chr
       raise StandardError unless letter.match(/[a-z]/)
     rescue StandardError
       raise 'Invalid Input!'
@@ -26,5 +28,21 @@ class Hangman
 
   attr_reader :key
 
-  attr_accessor :state, :wrong_guesses, :attempts_left
+  attr_writer :state, :wrong_guesses, :attempts_left
+
+  def evaluate(letter)
+    return false if already_guessed?(letter) || attempts_left < 1
+
+    found = false
+    key.each.with_index do |column, index|
+      state[index] = key[index] if column == key[index]
+      found = true
+    end
+    wrong_guesses.push(letter) unless found
+    true
+  end
+
+  def already_guessed?(letter)
+    state.any? == letter || wrong_guesses.any? == letter
+  end
 end

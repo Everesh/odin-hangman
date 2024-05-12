ba3332a7ce437a9de512dfe1815731327a993e1f
Everesh <everesh@proton.me>
2024-05-12 22:50:13 +0200

    Implement guess

--- hangman.rb
+++ hangman.rb
@@ -6,10 +6,25 @@ class Hangman
     dictionary = File.readlines('./dictionary.txt')
     @key = dictionary[rand(dictionary.size)]
     @state = Array.new(key.size)
+    @wrong_guesses = []
     @attempts_left = ATTEMPTS
+  end
+
+  def guess(letter)
+    letter = letter.downcase.char
+    begin
+      raise StandardError unless letter.match(/[a-z]/)
+    rescue StandardError
+      puts 'Invalid Input!'
+      return false
+    end
+    evaluate(letter)
+    true
   end
 
   private
 
   attr_reader :key
+
+  attr_accessor :state, :wrong_guesses, :attempts_left
 end
